62c4a50580b3cc7502b5c0ad1353460bdbf95409
谭引平 <yinp.tan001@bkjk.com>
2020-10-10 22:34:53 +0800

    feat：添加笔记

---
+++ plopfile.js
@@ -0,0 +1,50 @@
+const promptDirectory = require('inquirer-directory');
+module.exports = (plop) => {
+  plop.setPrompt('directory', promptDirectory);
+  plop.setGenerator('add page', {
+    description: 'create a page',
+    prompts: [
+      {
+        type: 'input',
+        name: 'pageName',
+        message: 'pageName',
+      },
+      {
+        type: 'confirm',
+        name: 'hasToc',
+        default: true,
+        message: 'hasToc?',
+      },
+      {
+        type: 'list',
+        name: 'sidebarDepth',
+        message: 'sidebarDepth:',
+        choices: ['0', '1'],
+      },
+      {
+        type: 'directory',
+        name: 'directory',
+        message: 'directory:',
+        basePath: './docs',
+      },
+    ],
+    actions: function(data) {
+      const { pageName, directory } = data;
+      const actions = [];
+      if (pageName && directory) {
+        console.log(directory)
+        actions.push({
+          type: 'add',
+          path: 'docs/{{directory}}/{{pageName}}.md',
+          templateFile: 'templates/index.txt',
+        });
+        actions.push({
+          type: 'add',
+          path: 'docs/{{directory}}/{{pageName}}.md',
+          templateFile: 'templates/index.txt',
+        });
+      }
+      return actions;
+    },
+  });
+};
