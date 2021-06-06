97a0ffb689131d1af6a8d84f40ff84ec2daa532e
tanyplzu <48279933+tanyplzu@users.noreply.github.com>
2021-06-06 15:28:38 +0800

    笔记

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -1,14 +1,15 @@
 <template>
   <ParentLayout>
+    <!-- <div slot="sidebar-bottom">底部信息</div> -->
   </ParentLayout>
 </template>
 
 <script>
-import ParentLayout from '@parent-theme/layouts/Layout.vue'
+import ParentLayout from "@parent-theme/layouts/Layout.vue";
 export default {
-  name: 'Layout',
+  name: "Layout",
   components: {
     ParentLayout,
-  }
-}
+  },
+};
 </script>
