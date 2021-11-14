c1ece4500436fd1499b3c66bfdd79e66d19596ec
tanyp <759227027@qq.com>
2021-11-15 00:55:41 +0800

    feat: 笔记更新

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: 'front-end-notes',
+      script: 'server.js --name react01'
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'root',
+      host: '172.24.26.134',
+      ref: 'origin/master',
+      repo: 'git@github.com:tanyplzu/front-end-notes.git',
+      path: '/www/website/front-end-notes',
+      ssh_options: 'StrictHostKeyChecking=no',
+      'post-deploy':
+        'git checkout master && git pull && npm i --production=false && npm run docs:build && pm2 startOrReload ecosystem.config.js',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  },
+  }
+}
