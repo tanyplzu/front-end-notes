284b856da0d6b1e8e7d4f84cbe95b9119140a6ed
tanyp <759227027@qq.com>
2021-11-15 21:45:54 +0800

    feat: 笔记更新

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
       path: '/www/website/front-end-notes',
       ssh_options: 'StrictHostKeyChecking=no',
       'post-deploy':
-        'git checkout master && git pull && npm i --production=false && npm run build && pm2 startOrReload ecosystem.config.js',
+        'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build && pm2 startOrReload ecosystem.config.js',
       env: {
         NODE_ENV: 'production',
       },
